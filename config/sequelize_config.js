d65edcdee812261a2a91cb6fb212fa34e6554578
Callanthas <lord_of_light1@hotmail.com>
2019-02-09 00:57:09 -0600

    heroku

--- config/sequelize_config.js
+++ config/sequelize_config.js
@@ -17,10 +17,7 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_URL,
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
--- server.js
+++ server.js
@@ -25,6 +25,16 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+/*if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}  else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'demonR3@lm',
+    database: 'class_project'
+  })
+} */
 //Tell sequelize to sync our models to our database
 //this executes pending migrations if we have any.
 //We use a .then() to make sure that our server won't
